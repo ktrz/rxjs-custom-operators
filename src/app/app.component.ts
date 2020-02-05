8da2d45282c5bd4f397f6b79c24bd59292ba9c2c
Chris Trześniewski <krzysztof.trzesniewski@scalac.io>
2020-02-05 11:33:11 +0100

    * implemented custom operators during presentation on Angular Berlin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {OperatorFunction} from 'rxjs';
-import {customMapClass, customMapFunc} from './operators';
 import {bufferDelayClass, bufferDelayFunc} from './operators/sandbox';
 
 @Component({
@@ -8,17 +7,15 @@ import {bufferDelayClass, bufferDelayFunc} from './operators/sandbox';
   template: `
     <app-operator-showcase-header></app-operator-showcase-header>
     <app-operator-showcase-content
-            *ngFor="let showcase of operatorsShowcases"
-            [header]="showcase.header"
-            [operators]="showcase.operators"
+      *ngFor="let showcase of operatorsShowcases"
+      [header]="showcase.header"
+      [operators]="showcase.operators"
     ></app-operator-showcase-content>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   operatorsShowcases: { header: string, operators: OperatorFunction<any, any>[] }[] = [
-    {header: 'Custom Map 1 (f-${x})', operators: [customMapFunc(s => `f-${s}`)]},
-    {header: 'Custom Map 2 (c-${x})', operators: [customMapClass(s => `c-${s}`)]},
     {header: 'Buffer Delay Func', operators: [bufferDelayFunc(1000)]},
     {header: 'Buffer Delay Class', operators: [bufferDelayClass(1000)]},
   ];
--- src/app/operators/sandbox.ts
+++ src/app/operators/sandbox.ts
@@ -1,11 +1,26 @@
-import {MonoTypeOperatorFunction, Observable, Operator, Subscriber, TeardownLogic} from 'rxjs';
+import {BehaviorSubject, MonoTypeOperatorFunction, Observable, Operator, Subscriber, TeardownLogic, zip} from 'rxjs';
+import {filter, map, tap} from 'rxjs/operators';
+import {notNull} from './not-null.operator';
 
 
 // function based implementation
 
 export const bufferDelayFunc = <T>(time: number): MonoTypeOperatorFunction<T> => (source: Observable<T>): Observable<T> => {
   return new Observable<T>(observer => {
-    source.subscribe(observer);
+    const openSubject = new BehaviorSubject(true);
+    const open$ = openSubject.asObservable().pipe(filter(v => v));
+
+    return zip(source, open$)
+      .pipe(
+        tap(() => {
+            openSubject.next(false);
+            setTimeout(() => {
+              openSubject.next(true);
+            }, time);
+          }
+        ),
+        map(([src]) => src)
+      ).subscribe(observer);
   });
 };
 
@@ -28,8 +43,31 @@ class BufferDelayOperator<T> implements Operator<T, T> {
 }
 
 class BufferDelaySubscriber<T> extends Subscriber<T> {
+  private sourceSubject = new BehaviorSubject(null);
+  private source = this.sourceSubject.asObservable().pipe(notNull);
+  private openSubject = new BehaviorSubject(true);
+  private open$ = this.openSubject.asObservable().pipe(filter(v => v));
+
+
   constructor(destination: Subscriber<T>, private time: number) {
     super(destination);
+
+    zip(this.source, this.open$)
+      .pipe(
+        tap(() => {
+            this.openSubject.next(false);
+            setTimeout(() => {
+              this.openSubject.next(true);
+            }, time);
+          }
+        ),
+        map(([src]) => src)
+      ).subscribe(destination);
+  }
+
+
+  protected _next(value) {
+    this.sourceSubject.next(value);
   }
 }
 
